6c41cbd60202c03f052c28fb12977f7633468c56
Chris L <chris.lichter@gmail.com>
2013-02-28 11:59:40 -0600

    I have created a multithreaded client. It is very limited at this time but it works. I plan on cleaning it up and making it more robust this weekend

--- initialScriptClient.py
+++ initialScriptClient.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python
 import socket
 import sys
+import datetime
+import threading
+from ping import Ping
+
+exitFlag=0
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter):
+    	threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        threading.Thread.__init__(self)
+    def run(self):
+        print "Starting " + self.name + '\n'
+        Ping(5)
+        print "Exiting " + self.name + '\n'
+
 
 #Create a TCP/IP socket
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,6 +44,14 @@ try:
 		data = soc.recv(150)
 		amount_received += len(data)
 		print >>sys.stderr, '\nReceived: "%s"' % data
+	thread1 = myThread(1, "Thread-1", 1)
+	thread2 = myThread(2, "Thread-2", 2)
+
+	# Start new Threads
+	thread1.start()
+	thread2.start()
+
+	print "Exiting Main Thread"
 	
 finally:
 	print >>sys.stderr, '\nClosing Socket'
--- ping.py
+++ ping.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 import os, sys
 
-pingAddress='172.18.69.236'
-pingCmd='ping -c18 %s | grep "potato"' % pingAddress
+threadCount=10
 
-os.system(pingCmd)
-print('Good Job')
+def Ping(threadCount):
+	pingAddress='127.0.0.1'
+	#pingCmd='ping -c18 %s | grep "potato"' % pingAddress
+	pingCmd='ping -c2 %s > /dev/null 2>&1' % pingAddress
+	os.system(pingCmd)
+	print('Good Job')
+	exit()
