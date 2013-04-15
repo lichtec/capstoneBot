cb534617fc7f9e9bd1d568c5f9254a9b81ce833f
Chris L <chris.lichter@gmail.com>
2013-04-15 14:00:55 -0500

    test.py with simple while loop

--- test.py
+++ test.py
@@ -1,7 +1,37 @@
 import httplib
-host = 'ArchMO16'
+import threading
+
+# class threadClass(threading.Thread):
+# 	def __init__(self, threadID, name, counter):
+# 		threading.Thread.__init__(self)
+# 		self.threadID = threadID
+# 		self.name = name
+# 		self.counter = counter
+# 	def run(self):
+# 		test()
+
+host = 'www.python.org'
 port = 80
-conn = httplib.HTTPConnection(host, port)
-conn.request("Get", "/index.html")
-r1 = conn.getresponse()
-print r1.status, r1.reasonpy                   
+		
+x=5
+while(x>0):
+	conn = httplib.HTTPConnection(host, port)
+	conn.request("GET", "/index.html")
+	r1 = conn.getresponse()
+	print x, r1.status, r1.reason, r1.msg
+	x=x-1
+	
+# thread1 = threadClass(1, "Thread-1", 1)
+# thread2 = threadClass(2, "Thread-2", 2)
+# 
+# thread1.start()
+# thread2.start()
+# 
+# threads.append(thread1)
+# threads.append(thread2)
+# 
+# for t in threads:
+# 	t.join()
+# 
+# print "Exiting Main Thread"
+#                    
