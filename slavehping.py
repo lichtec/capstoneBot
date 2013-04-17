663c189fbed141d8da0d112516e14e6fba2b8fa8
drinkard <eric.drinkard@washburn.edu>
2013-04-17 12:54:09 -0500

    Fixed hping

--- slavehping.py
+++ slavehping.py
@@ -2,7 +2,7 @@ import os
 #from sys import argv
 
 def Hping(ipAdd, dest_addr):
-	hpingCmd = 'sudo hping -i -u1 -S -p80 %s' % (dest_addr)
+	hpingCmd = 'sudo hping3 -i -u1 -S -p 80 %s' % (dest_addr)
 	os.system(hpingCmd)
 	print('\nDone')
 	exit()
